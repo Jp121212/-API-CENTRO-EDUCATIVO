8ccd86a3b626e72ac3d4110b1466ef5300b8f086
Jp121212 <jose.elizondo@unadeca.net>
2022-07-01 18:37:32 -0600

    Se agrega la ruta matricula.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import estudianteRoutes from "./routes/estudiante.routes.js";
 import materiaRoutes from "./routes/materia.routes.js";
 import profesoreRoutes from "./routes/profesor.routes.js";
 import facultadRoutes from "./routes/facultad.routes.js";
+import matriculasRoutes from "./routes/matricula.routes.js";
 import morgan from "morgan";
 
 const app = express()
@@ -17,6 +18,7 @@ app.use(estudianteRoutes);
 app.use(materiaRoutes);
 app.use(profesoreRoutes);
 app.use(facultadRoutes);
+app.use(matriculasRoutes);
 
 app.get('/', (req, res) => {
   res.send('- Si sirve la API 👩 -')
